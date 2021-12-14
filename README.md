83be5f54bae8e424ad050155743707524027aed5
Will Hartman <me@willhartman.co.uk>
2021-12-14 01:12:05 +0000

    feat(aoc): Implement SonarDepth Measurement Windows

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Advent of Code 2021
 
-### Day 1
-### [Challenge](https://adventofcode.com/2021/day/1) / [Solution](./day-1)
+## Day 1
+### Part 1 / [Challenge](https://adventofcode.com/2021/day/1) / [Solution](https://github.com/willhartman/advent-of-code/commit/926597e9f9a0bd26ec2e7ba5fb5347ef8ca0a602)
+
+### Part 2 / [Challenge](https://adventofcode.com/2021/day/1#part2) / [Solution](./day-1)
--- day-1/src/sonar-depth/sonar-depth.ts
+++ day-1/src/sonar-depth/sonar-depth.ts
@@ -1,6 +1,31 @@
 export class SonarDepth {
   constructor(public value: number, public next?: SonarDepth) {}
 
+  sumWithNextN(quantity: number = 2): number {
+    return (
+      this.value +
+      +(
+        quantity &&
+        this.next !== undefined &&
+        this.next?.sumWithNextN(quantity - 1)
+      )
+    );
+  }
+
+  get threeMeasurementWindow(): number {
+    return this.sumWithNextN(2);
+  }
+
+  get threeMeasurementWindowIncreases(): number {
+    return (
+      +this.threeMeasurementWindowIncreasingNext + +(this.next !== undefined && this.next?.threeMeasurementWindowIncreases)
+    );
+  }
+
+  get threeMeasurementWindowIncreasingNext(): boolean {
+    return this.next !== undefined && this.next.threeMeasurementWindow > this.threeMeasurementWindow;
+  }
+
   get increases(): number {
     return (
       +this.increasingNext + +(this.next !== undefined && this.next?.increases)
